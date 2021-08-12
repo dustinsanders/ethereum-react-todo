06a23578bb12b4ef813917b3f1b46a9a3b284549
Dustin Sanders <dustinsanders91@gmail.com>
2021-08-12 17:33:18 -0500

    reverting deploy changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,11 @@
 const { ethers } = require('hardhat')
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  console.log('Deploying contracts with the account:', deployer.address)
+  console.log('Account balance:', (await deployer.getBalance()).toString())
+
   const Todo = await ethers.getContractFactory('Todo')
   const todo = await Todo.deploy()
 
