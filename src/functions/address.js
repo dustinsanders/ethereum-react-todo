f716c8ba6f3b576b96b9a1e30f4b9244bc876183
Dustin Sanders <dustinsanders91@gmail.com>
2021-08-13 13:47:29 -0500

    another env test

--- src/functions/address.js
+++ src/functions/address.js
@@ -1,33 +1,33 @@
-const safeRequire = require('safe-require')
-const local = safeRequire('./config/local')
-const testnet = require('./config/testnet')
-const get = require('lodash/get')
-const { utils } = require('ethers')
+// const safeRequire = require('safe-require')
+// const local = safeRequire('./config/local')
+// const testnet = require('./config/testnet')
+// const get = require('lodash/get')
+// const { utils } = require('ethers')
 
-const getValue = queryStringParameters => {
-  const {
-    CONTRACT_ADDRESS,
-    NODE_ENV,
-  } = process.env
+// const getValue = queryStringParameters => {
+//   const {
+//     CONTRACT_ADDRESS,
+//     NODE_ENV,
+//   } = process.env
 
-  if (get(queryStringParameters, 'net') === 'testnet') {
-    return testnet
-  }
+//   if (get(queryStringParameters, 'net') === 'testnet') {
+//     return testnet
+//   }
 
-  if (CONTRACT_ADDRESS && utils.isAddress(CONTRACT_ADDRESS)) {
-    return CONTRACT_ADDRESS
-  }
+//   if (CONTRACT_ADDRESS && utils.isAddress(CONTRACT_ADDRESS)) {
+//     return CONTRACT_ADDRESS
+//   }
 
-  return NODE_ENV === 'production'
-    ? testnet
-    : local
-}
+//   return NODE_ENV === 'production'
+//     ? testnet
+//     : local
+// }
 
 exports.handler = async ({ queryStringParameters }) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      value: getValue(queryStringParameters),
+      // value: getValue(queryStringParameters),
       env: process.env.NODE_ENV,
     })
   }
