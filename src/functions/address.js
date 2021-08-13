a69c4f2b0caf99f79ca96687fb04d339f169c950
Dustin Sanders <dustinsanders91@gmail.com>
2021-08-13 14:04:01 -0500

    checking netlify_dev

--- src/functions/address.js
+++ src/functions/address.js
@@ -1,37 +1,35 @@
-// const safeRequire = require('safe-require')
-// const local = safeRequire('./config/local')
-// const testnet = require('./config/testnet')
-// const get = require('lodash/get')
-// const { utils } = require('ethers')
+const safeRequire = require('safe-require')
+const local = safeRequire('./config/local')
+const testnet = require('./config/testnet')
+const get = require('lodash/get')
+const { utils } = require('ethers')
 
-// const getValue = queryStringParameters => {
-//   const {
-//     CONTRACT_ADDRESS,
-//     NODE_ENV,
-//   } = process.env
+const getValue = queryStringParameters => {
+  const {
+    CONTRACT_ADDRESS,
+    NETLIFY_DEV,
+  } = process.env
 
-//   if (get(queryStringParameters, 'net') === 'testnet') {
-//     return testnet
-//   }
+  if (get(queryStringParameters, 'net') === 'testnet') {
+    return testnet
+  }
 
-//   if (CONTRACT_ADDRESS && utils.isAddress(CONTRACT_ADDRESS)) {
-//     return CONTRACT_ADDRESS
-//   }
+  if (CONTRACT_ADDRESS && utils.isAddress(CONTRACT_ADDRESS)) {
+    return CONTRACT_ADDRESS
+  }
 
-//   return NODE_ENV === 'production'
-//     ? testnet
-//     : local
-// }
+  return NETLIFY_DEV === 'true'
+    ? local
+    : testnet
+}
 
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 
-exports.handler = async ({ queryStringParameters }, context) => {
+exports.handler = async ({ queryStringParameters }) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      // value: getValue(queryStringParameters),
-      env: Object.keys(process.env),
-      context: Object.keys(process.env),
+      value: getValue(queryStringParameters),
     })
   }
 }
